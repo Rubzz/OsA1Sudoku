05b9ca8a9b630a406cab1aff4f70095c95d2e47f
Isabelle <rubenkobb@gmail.com>
2022-03-15 13:34:35 +0100

    Sudoku solver docs redux

--- src/main/java/Group14/SudokuSolver/SudokuSolver.java
+++ src/main/java/Group14/SudokuSolver/SudokuSolver.java
@@ -22,7 +22,11 @@ public class SudokuSolver implements Runnable  {
         this.sudoku.add("9");
     }
 
-
+    /**
+     * Checks an array of 9 strings if they contain the numbers from 1-9 and returns true if they do.
+     * @param sudokuLine the array to check
+     * @return true or false based on if the array contains 1-9 or not
+     */
     public boolean checkSolveLine(ArrayList<String> sudokuLine) {
         ArrayList<String> line = sudokuLine;
         if (line.contains("1") && line.contains("2") && line.contains("3") &&
@@ -37,6 +41,11 @@ public class SudokuSolver implements Runnable  {
         }
     }
 
+    /**
+     * Sends a row to be checked for the numbers 1-9
+     * @param row the row to check
+     * @return true if the row contains the numbers 1-9
+     */
     public boolean checkRow(int row)  {
         ArrayList<String> sudokuLine = new ArrayList<String>();
         int x = (row-1);
@@ -52,6 +61,11 @@ public class SudokuSolver implements Runnable  {
         return checkSolveLine(sudokuLine);
     }
 
+    /**
+     * Sends a column to be checked for the numbers 1-9
+     * @param column the column to check
+     * @return true if the column contains the numbers 1-9
+     */
     public boolean checkColumn(int column) {
         ArrayList<String> sudokuLine = new ArrayList<String>();
         int x = (column-1);
@@ -67,9 +81,14 @@ public class SudokuSolver implements Runnable  {
         return checkSolveLine(sudokuLine);
     }
 
-    public boolean checkBox123(int boxColumn)  {
+    /**
+     * Sends a box to be checked for the numbers 1-9
+     * @param box the box to check
+     * @return true if the box contains the numbers 1-9
+     */
+    public boolean checkBox123(int box)  {
         ArrayList<String> sudokuLine = new ArrayList<String>();
-        int x = (boxColumn-1);
+        int x = (box-1);
         sudokuLine.add(this.sudoku.get(0+(3*x)));
         sudokuLine.add(this.sudoku.get(1+(3*x)));
         sudokuLine.add(this.sudoku.get(2+(3*x)));
@@ -82,9 +101,14 @@ public class SudokuSolver implements Runnable  {
         return checkSolveLine(sudokuLine);
     }
 
-    public boolean checkBox456(int boxColumn)  {
+    /**
+     * Sends a box to be checked for the numbers 1-9
+     * @param box the box to check
+     * @return true if the box contains the numbers 1-9
+     */
+    public boolean checkBox456(int box)  {
         ArrayList<String> sudokuLine = new ArrayList<String>();
-        int x = (boxColumn-1);
+        int x = (box-4);
         sudokuLine.add(this.sudoku.get(27+(3*x)));
         sudokuLine.add(this.sudoku.get(28+(3*x)));
         sudokuLine.add(this.sudoku.get(29+(3*x)));
@@ -97,9 +121,14 @@ public class SudokuSolver implements Runnable  {
         return checkSolveLine(sudokuLine);
     }
 
-    public boolean checkBox789(int boxColumn)  {
+    /**
+     * Sends a box to be checked for the numbers 1-9
+     * @param box the box to check
+     * @return true if the box contains the numbers 1-9
+     */
+    public boolean checkBox789(int box)  {
         ArrayList<String> sudokuLine = new ArrayList<String>();
-        int x = (boxColumn-1);
+        int x = (box-7);
         sudokuLine.add(this.sudoku.get(54+(3*x)));
         sudokuLine.add(this.sudoku.get(55+(3*x)));
         sudokuLine.add(this.sudoku.get(56+(3*x)));
@@ -112,6 +141,10 @@ public class SudokuSolver implements Runnable  {
         return checkSolveLine(sudokuLine);
     }
 
+    /**
+     * Calls to check every single row in the sudoku
+     * @return true if all rows contains the numbers 1-9
+     */
     public boolean checkRows() {
         if (checkRow(1) && checkRow(2) && checkRow(3) && checkRow(4) && checkRow(5) && checkRow(6) &&
                 checkRow(7) && checkRow(8) && checkRow(9))
@@ -119,6 +152,10 @@ public class SudokuSolver implements Runnable  {
         else return false;
     }
 
+    /**
+     * Calls to check every single column in the sudoku
+     * @return true if all columns contains the numbers 1-9
+     */
         public boolean checkColumns() {
             if (checkColumn(1) && checkColumn(2) && checkColumn(3) &&
         checkColumn(4) && checkColumn(5) && checkColumn(6) && checkColumn(7) && checkColumn(8) && checkColumn(9))
@@ -126,10 +163,14 @@ public class SudokuSolver implements Runnable  {
         else return false;
     }
 
+    /**
+     * Calls to check every single box in the sudoku
+     * @return true if all boxes contains the numbers 1-9
+     */
     public boolean checkBoxes() {
         if (checkBox123(1) && checkBox123(2) && checkBox123(3) &&
-                checkBox456(1) && checkBox456(2) && checkBox456(3) &&
-                checkBox789(1) && checkBox789(2) && checkBox789(3))
+                checkBox456(4) && checkBox456(5) && checkBox456(6) &&
+                checkBox789(7) && checkBox789(8) && checkBox789(9))
             return true;
         else return false;
     }
