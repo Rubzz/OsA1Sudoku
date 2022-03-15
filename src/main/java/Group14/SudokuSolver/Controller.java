2fe73b8d357171e979efbcb2104a5310e8f91df9
alegro91 <alegro91@users.noreply.github.com>
2022-03-15 12:32:34 +0100

    threads, csv fix etc
    barebones implementation of threads, WIP. changed csv reader format to csv

--- src/main/java/Group14/SudokuSolver/Controller.java
+++ src/main/java/Group14/SudokuSolver/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
+
 import java.io.File;
 import java.io.IOException;
 
@@ -17,7 +18,7 @@ import java.io.IOException;
  */
 
 
-public class Controller {
+public class Controller implements Runnable {
     private static String version = "V 0.1-SNAPSHOT";
     private static String application = "SudokuSolver";
 
@@ -29,7 +30,6 @@ public class Controller {
     public void initialize()
     {
     }
-
 
     /**
      *Clears the existing table and creates a dialog for importing .txt files
@@ -43,7 +43,7 @@ public class Controller {
         Scene scene = new Scene(new Group());
         Window chooseDialogWindow = scene.getWindow();
         FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text file format value (*.txt)", "*.txt");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Comma-delimited value (*.csv)", "*.csv");
         fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
         fileChooser.getExtensionFilters().add(extFilter);
         File file = fileChooser.showOpenDialog(chooseDialogWindow);
@@ -60,6 +60,25 @@ public class Controller {
     public void parseFromFile(File file) throws IOException {
         CSV_Reader fileReader = new CSV_Reader();
         sudokuSolver.mergeArrayLists(fileReader.fileParseToArrayList(file));
+    }
+
+    @Override
+    public void run() {
+        Thread thread1 =new Thread(sudokuSolver.checkRow());
+        thread1.start();
+        thread1.run();
+
+        Thread thread2 =new Thread(sudokuSolver.checkColumn());
+        thread2.start();
+        thread2.run();
+
+        Thread thread3 =new Thread(sudokuSolver.checkBoxes());
+        thread3.start();
+        thread3.run();
+
+
+
+
     }
 
 }
--- src/main/java/Group14/SudokuSolver/SudokuSolver.java
+++ src/main/java/Group14/SudokuSolver/SudokuSolver.java
@@ -2,7 +2,7 @@ package Group14.SudokuSolver;
 
 import java.util.ArrayList;
 
-public class SudokuSolver implements Runnable {
+public class SudokuSolver implements Runnable  {
     private ArrayList<String> sudoku = new ArrayList<>();
 
     private void addNumber(int number) {
@@ -21,22 +21,23 @@ public class SudokuSolver implements Runnable {
         this.sudoku.add("9");
     }
 
-    private boolean checkSolveLine(String sudokuLine) {
-        new Thread(new SudokuSolver()).start();
+
+    public boolean checkSolveLine(String sudokuLine) {
+
         String line = sudokuLine;
         if (line.contains("1") && line.contains("2") && line.contains("3") &&
                 line.contains("4") && line.contains("5") && line.contains("6") &&
                 line.contains("7") && line.contains("8") && line.contains("9")) {
-            System.out.println("line is correct");
+            System.out.println("line is valid");
             return true;
         }
         else  {
-            System.out.println("line is fake");
+            System.out.println("line is invalid");
             return false;
         }
     }
 
-    private boolean checkRow(int row)  {
+    public boolean checkRow(int row)  {
         String sudokuLine = new String();
         int x = (row-1);
             sudokuLine = this.sudoku.get(0+(9*x)) + this.sudoku.get(1+(9*x)) + this.sudoku.get(2+(9*x)) +
@@ -45,7 +46,7 @@ public class SudokuSolver implements Runnable {
             return checkSolveLine(sudokuLine);
     }
 
-    private boolean checkColumn(int column) {
+    public boolean checkColumn(int column) {
         String sudokuLine = new String();
         int x = (column-1);
         sudokuLine = this.sudoku.get(0+x) + this.sudoku.get(9+x) + this.sudoku.get(18+x) +
@@ -54,7 +55,7 @@ public class SudokuSolver implements Runnable {
         return checkSolveLine(sudokuLine);
     }
 
-    private boolean checkBox123(int boxColumn)  {
+    public boolean checkBox123(int boxColumn)  {
         String sudokuLine = new String();
         int x = (boxColumn-1);
         sudokuLine = this.sudoku.get(0+(3*x)) + this.sudoku.get(1+(3*x)) + this.sudoku.get(2+(3*x)) +
@@ -63,7 +64,7 @@ public class SudokuSolver implements Runnable {
         return checkSolveLine(sudokuLine);
     }
 
-    private boolean checkBox456(int boxColumn)  {
+    public boolean checkBox456(int boxColumn)  {
         String sudokuLine = new String();
         int x = (boxColumn-1);
         sudokuLine = this.sudoku.get(27+(3*x)) + this.sudoku.get(28+(3*x)) + this.sudoku.get(29+(3*x)) +
@@ -72,7 +73,7 @@ public class SudokuSolver implements Runnable {
         return checkSolveLine(sudokuLine);
     }
 
-    private boolean checkBox789(int boxColumn)  {
+    public boolean checkBox789(int boxColumn)  {
         String sudokuLine = new String();
         int x = (boxColumn-1);
         sudokuLine = this.sudoku.get(54+(3*x)) + this.sudoku.get(55+(3*x)) + this.sudoku.get(56+(3*x)) +
@@ -81,28 +82,29 @@ public class SudokuSolver implements Runnable {
         return checkSolveLine(sudokuLine);
     }
 
-    public boolean checkSudoku(ArrayList<String> sudokuToSolve)    {
-        this.sudoku = sudokuToSolve;
-        if (checkRow(1) && checkRow(2) && checkRow(3) && checkRow(4) && checkRow(5) && checkRow(6) &&
-        checkRow(7) && checkRow(8) && checkRow(9) && checkColumn(1) && checkColumn(2) && checkColumn(3) &&
-        checkColumn(4) && checkColumn(5) && checkColumn(6) && checkColumn(7) && checkColumn(8) && checkColumn(9) &&
-        checkBox123(1) && checkBox123(2) && checkBox123(3) &&
-        checkBox456(1) && checkBox456(2) && checkBox456(3) &&
-        checkBox789(1) && checkBox789(2) && checkBox789(3))
-            return true;
-        else return false;
-    }
+//    public boolean checkSudoku(ArrayList<String> sudokuToSolve)    {
+//        this.sudoku = sudokuToSolve;
+//        if (checkRow(1) && checkRow(2) && checkRow(3) && checkRow(4) && checkRow(5) && checkRow(6) &&
+//        checkRow(7) && checkRow(8) && checkRow(9) && checkColumn(1) && checkColumn(2) && checkColumn(3) &&
+//        checkColumn(4) && checkColumn(5) && checkColumn(6) && checkColumn(7) && checkColumn(8) && checkColumn(9) &&
+//        checkBox123(1) && checkBox123(2) && checkBox123(3) &&
+//        checkBox456(1) && checkBox456(2) && checkBox456(3) &&
+//        checkBox789(1) && checkBox789(2) && checkBox789(3))
+//            return true;
+//        else return false;
+//    }
+//
+//    public boolean checkSudoku()    {
+//        if (checkRow(1) && checkRow(2) && checkRow(3) && checkRow(4) && checkRow(5) && checkRow(6) &&
+//                checkRow(7) && checkRow(8) && checkRow(9) && checkColumn(1) && checkColumn(2) && checkColumn(3) &&
+//                checkColumn(4) && checkColumn(5) && checkColumn(6) && checkColumn(7) && checkColumn(8) && checkColumn(9) &&
+//                checkBox123(1) && checkBox123(2) && checkBox123(3) &&
+//                checkBox456(1) && checkBox456(2) && checkBox456(3) &&
+//                checkBox789(1) && checkBox789(2) && checkBox789(3))
+//            return true;
+//        else return false;
+//    }
 
-    public boolean checkSudoku()    {
-        if (checkRow(1) && checkRow(2) && checkRow(3) && checkRow(4) && checkRow(5) && checkRow(6) &&
-                checkRow(7) && checkRow(8) && checkRow(9) && checkColumn(1) && checkColumn(2) && checkColumn(3) &&
-                checkColumn(4) && checkColumn(5) && checkColumn(6) && checkColumn(7) && checkColumn(8) && checkColumn(9) &&
-                checkBox123(1) && checkBox123(2) && checkBox123(3) &&
-                checkBox456(1) && checkBox456(2) && checkBox456(3) &&
-                checkBox789(1) && checkBox789(2) && checkBox789(3))
-            return true;
-        else return false;
-    }
 
     public void mergeArrayLists(ArrayList<String> mergeList)
     {
@@ -114,90 +116,95 @@ public class SudokuSolver implements Runnable {
         return sudoku;
     }
 
-     public void run()  {
-        System.out.println("My first thread is running…");
-     }
-    public void addCorrectSudoku() {
-        this.sudoku.add("9");
-        this.sudoku.add("2");
-        this.sudoku.add("5");
-        this.sudoku.add("6");
-        this.sudoku.add("3");
-        this.sudoku.add("1");
-        this.sudoku.add("8");
-        this.sudoku.add("4");
-        this.sudoku.add("7");
-        this.sudoku.add("6");
-        this.sudoku.add("1");
-        this.sudoku.add("8");
-        this.sudoku.add("5");
-        this.sudoku.add("7");
-        this.sudoku.add("4");
-        this.sudoku.add("2");
-        this.sudoku.add("9");
-        this.sudoku.add("3");
-        this.sudoku.add("3");
-        this.sudoku.add("7");
-        this.sudoku.add("4");
-        this.sudoku.add("9");
-        this.sudoku.add("8");
-        this.sudoku.add("2");
-        this.sudoku.add("5");
-        this.sudoku.add("6");
-        this.sudoku.add("1");
-        this.sudoku.add("7");
-        this.sudoku.add("4");
-        this.sudoku.add("9");
-        this.sudoku.add("8");
-        this.sudoku.add("2");
-        this.sudoku.add("6");
-        this.sudoku.add("1");
-        this.sudoku.add("3");
-        this.sudoku.add("5");
-        this.sudoku.add("8");
-        this.sudoku.add("5");
-        this.sudoku.add("2");
-        this.sudoku.add("4");
-        this.sudoku.add("1");
-        this.sudoku.add("3");
-        this.sudoku.add("9");
-        this.sudoku.add("7");
-        this.sudoku.add("6");
-        this.sudoku.add("1");
-        this.sudoku.add("6");
-        this.sudoku.add("3");
-        this.sudoku.add("7");
-        this.sudoku.add("9");
-        this.sudoku.add("5");
-        this.sudoku.add("4");
-        this.sudoku.add("8");
-        this.sudoku.add("2");
-        this.sudoku.add("2");
-        this.sudoku.add("8");
-        this.sudoku.add("7");
-        this.sudoku.add("3");
-        this.sudoku.add("5");
-        this.sudoku.add("9");
-        this.sudoku.add("6");
-        this.sudoku.add("1");
-        this.sudoku.add("4");
-        this.sudoku.add("4");
-        this.sudoku.add("9");
-        this.sudoku.add("1");
-        this.sudoku.add("2");
-        this.sudoku.add("6");
-        this.sudoku.add("7");
-        this.sudoku.add("3");
-        this.sudoku.add("5");
-        this.sudoku.add("8");
-        this.sudoku.add("5");
-        this.sudoku.add("3");
-        this.sudoku.add("6");
-        this.sudoku.add("1");
-        this.sudoku.add("4");
-        this.sudoku.add("8");
-        this.sudoku.add("7");
-        this.sudoku.add("2");
-        this.sudoku.add("9");
+    @Override
+    public void run() {
+
     }
+
+//     public void run()  {
+//        System.out.println("My first thread is running…");
+//     }
+//    public void addCorrectSudoku() {
+//        this.sudoku.add("9");
+//        this.sudoku.add("2");
+//        this.sudoku.add("5");
+//        this.sudoku.add("6");
+//        this.sudoku.add("3");
+//        this.sudoku.add("1");
+//        this.sudoku.add("8");
+//        this.sudoku.add("4");
+//        this.sudoku.add("7");
+//        this.sudoku.add("6");
+//        this.sudoku.add("1");
+//        this.sudoku.add("8");
+//        this.sudoku.add("5");
+//        this.sudoku.add("7");
+//        this.sudoku.add("4");
+//        this.sudoku.add("2");
+//        this.sudoku.add("9");
+//        this.sudoku.add("3");
+//        this.sudoku.add("3");
+//        this.sudoku.add("7");
+//        this.sudoku.add("4");
+//        this.sudoku.add("9");
+//        this.sudoku.add("8");
+//        this.sudoku.add("2");
+//        this.sudoku.add("5");
+//        this.sudoku.add("6");
+//        this.sudoku.add("1");
+//        this.sudoku.add("7");
+//        this.sudoku.add("4");
+//        this.sudoku.add("9");
+//        this.sudoku.add("8");
+//        this.sudoku.add("2");
+//        this.sudoku.add("6");
+//        this.sudoku.add("1");
+//        this.sudoku.add("3");
+//        this.sudoku.add("5");
+//        this.sudoku.add("8");
+//        this.sudoku.add("5");
+//        this.sudoku.add("2");
+//        this.sudoku.add("4");
+//        this.sudoku.add("1");
+//        this.sudoku.add("3");
+//        this.sudoku.add("9");
+//        this.sudoku.add("7");
+//        this.sudoku.add("6");
+//        this.sudoku.add("1");
+//        this.sudoku.add("6");
+//        this.sudoku.add("3");
+//        this.sudoku.add("7");
+//        this.sudoku.add("9");
+//        this.sudoku.add("5");
+//        this.sudoku.add("4");
+//        this.sudoku.add("8");
+//        this.sudoku.add("2");
+//        this.sudoku.add("2");
+//        this.sudoku.add("8");
+//        this.sudoku.add("7");
+//        this.sudoku.add("3");
+//        this.sudoku.add("5");
+//        this.sudoku.add("9");
+//        this.sudoku.add("6");
+//        this.sudoku.add("1");
+//        this.sudoku.add("4");
+//        this.sudoku.add("4");
+//        this.sudoku.add("9");
+//        this.sudoku.add("1");
+//        this.sudoku.add("2");
+//        this.sudoku.add("6");
+//        this.sudoku.add("7");
+//        this.sudoku.add("3");
+//        this.sudoku.add("5");
+//        this.sudoku.add("8");
+//        this.sudoku.add("5");
+//        this.sudoku.add("3");
+//        this.sudoku.add("6");
+//        this.sudoku.add("1");
+//        this.sudoku.add("4");
+//        this.sudoku.add("8");
+//        this.sudoku.add("7");
+//        this.sudoku.add("2");
+//        this.sudoku.add("9");
+//    }
 }
